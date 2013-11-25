f3112808a3005b94d07a4f7ecfd9f25c9e8cff7f
Stephen Rosen <sirosen@uchicago.edu>
2013-11-24 22:53:15 -0600

    Made test docstrings name util and config tests.
    This is a modification to accomodate the way that nose checks docstrings
    for test names.

--- tests/src/util/enum_test.py
+++ tests/src/util/enum_test.py
@@ -5,6 +5,11 @@ from nose.tools import istest
 
 @istest
 def automapping():
+    """
+    Enum Util Automapping
+    Tests that the automatic mapping of strings in an enum
+    correctly assigns them to themselves.
+    """
     x = enum.Enum('A','B')
     y = x.B
     assert(x.A == 'A')
@@ -12,6 +17,11 @@ def automapping():
 
 @istest
 def explicit_mapping():
+    """
+    Enum Util Explicit Mapping
+    Tests that the explicit mapping of attrs in an enum using kwargs
+    results in those kwargs being correctly assigned.
+    """
     def func(): pass
     x = enum.Enum(A=1,B='x.B',C=func)
     assert(x.A == 1)
@@ -20,6 +30,11 @@ def explicit_mapping():
 
 @istest
 def mixed_mapping():
+    """
+    Enum Util Mixed Mapping
+    Tests that a mixture of automatic and explicit assignments has the
+    desired effect.
+    """
     def func(): pass
     x = enum.Enum('A',B='x.B',C=func)
     assert(x.A == 'A')
--- tests/src/util/locations_test.py
+++ tests/src/util/locations_test.py
@@ -6,12 +6,22 @@ from mock import patch
 
 @istest
 def get_salve_root():
+    """
+    Locations Util Get SALVE Root Dir
+    Tests the path searching code for the SALVE root.
+    This is always relative to locations' __file__ attribute.
+    """
     with patch('src.util.locations.__file__',
                '/tmp/SALVE/src/util/locations.py'):
         assert src.util.locations.get_salve_root() == '/tmp/SALVE'
 
 @istest
 def get_default_config():
+    """
+    Locations Util Get Default Config File
+    Tests finding the settings ini file.
+    This is, like the SALVE root, always relative to locations.__file__
+    """
     with patch('src.util.locations.__file__',
                '/tmp/SALVE/src/util/locations.py'):
         assert src.util.locations.get_default_config() == \
