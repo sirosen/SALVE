8e53ecd4b801779484fe90de9b6aff8f9284fd51
Stephen Rosen <sirosen@uchicago.edu>
2013-12-14 14:56:54 -0600

    Refinements to scratch; more E2E run tests.
    Scratch function names and usage are now tidier. Wrote up an early
    backup test. Copies the manifest over an empty file and confirms
    that the file can still be found inside of the backups dir.

--- tests/end2end/run_file_test.py
+++ tests/end2end/run_file_test.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python
 
 from nose.tools import istest, with_setup
-import mock, os
+import mock, os, shlex
 
 import src.run.command
-import tests.utils.scratch as scratch
+import tests.utils.scratch
 
 def run_on_args(argv):
     with mock.patch('sys.argv',argv):
         return src.run.command.main()
 
-class TestWithScratchdir():
-    def setUp(self):
-        scratch.make_scratch(self)
-    def tearDown(self):
-        scratch.rm_scratch(self)
-
+class TestWithScratchdir(tests.utils.scratch.ScratchContainer):
     @istest
     def copy_single_file(self):
         """
@@ -26,11 +21,11 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         content = 'file { action copy source 1.man target 2.man }\n'
-        scratch.write_scratchfile(self,'1.man',content)
+        self.write_file('1.man',content)
         man_path = os.path.join(cwd,'1.man')
         run_on_args(['./salve.py','-m',man_path])
         assert os.path.exists(os.path.join(cwd,'2.man'))
-        s = scratch.read_scratchfile(self,'2.man')
+        s = self.read_file('2.man')
         assert s == content, '%s' % s
 
     @istest
@@ -43,11 +38,11 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         content = 'file { source 1.man target 2.man }\n'
-        scratch.write_scratchfile(self,'1.man',content)
+        self.write_file('1.man',content)
         man_path = os.path.join(cwd,'1.man')
         run_on_args(['./salve.py','-m',man_path])
         assert os.path.exists(os.path.join(cwd,'2.man'))
-        s = scratch.read_scratchfile(self,'2.man')
+        s = self.read_file('2.man')
         assert s == content, '%s' % s
 
     @istest
@@ -60,22 +55,22 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         man_path = os.path.join(cwd,'1.man')
-        scratch.write_scratchfile(self,'1.man',
+        self.write_file('1.man',
             'file { action copy source f1 target f1prime }\n'+\
             'file { source f2 target f2prime }\n'
             )
         f1_content = 'alpha beta\n'
         f2_content = 'gamma\ndelta\n'
-        scratch.write_scratchfile(self,'f1',f1_content)
-        scratch.write_scratchfile(self,'f2',f2_content)
+        self.write_file('f1',f1_content)
+        self.write_file('f2',f2_content)
 
         run_on_args(['./salve.py','-m',man_path])
 
         assert os.path.exists(os.path.join(cwd,'f1prime'))
         assert os.path.exists(os.path.join(cwd,'f2prime'))
-        s = scratch.read_scratchfile(self,'f1prime')
+        s = self.read_file('f1prime')
         assert s == f1_content, '%s' % s
-        s = scratch.read_scratchfile(self,'f2prime')
+        s = self.read_file('f2prime')
         assert s == f2_content, '%s' % s
 
     @istest
@@ -88,11 +83,11 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         content = 'file { action create target f2 }\n'
-        scratch.write_scratchfile(self,'1.man',content)
+        self.write_file('1.man',content)
         man_path = os.path.join(cwd,'1.man')
         run_on_args(['./salve.py','-m',man_path])
         assert os.path.exists(os.path.join(cwd,'f2'))
-        s = scratch.read_scratchfile(self,'f2')
+        s = self.read_file('f2')
         assert s == '', '%s' % s
 
     @istest
@@ -105,14 +100,14 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         content = 'file{action create target f2}\nfile{action\ncreate target f3}'
-        scratch.write_scratchfile(self,'1.man',content)
+        self.write_file('1.man',content)
         man_path = os.path.join(cwd,'1.man')
         run_on_args(['./salve.py','-m',man_path])
         assert os.path.exists(os.path.join(cwd,'f2'))
         assert os.path.exists(os.path.join(cwd,'f3'))
-        s = scratch.read_scratchfile(self,'f2')
+        s = self.read_file('f2')
         assert s == '', '%s' % s
-        s = scratch.read_scratchfile(self,'f3')
+        s = self.read_file('f3')
         assert s == '', '%s' % s
 
     @istest
@@ -125,13 +120,13 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         content = 'file{action create target f1}\n'
-        scratch.write_scratchfile(self,'1.man',content)
+        self.write_file('1.man',content)
         f1_content = 'alpha beta\n'
-        scratch.write_scratchfile(self,'f1',f1_content)
+        self.write_file('f1',f1_content)
         man_path = os.path.join(cwd,'1.man')
         run_on_args(['./salve.py','-m',man_path])
         assert os.path.exists(os.path.join(cwd,'f1'))
-        s = scratch.read_scratchfile(self,'f1')
+        s = self.read_file('f1')
         assert s == f1_content, '%s' % s
 
     @istest
@@ -144,13 +139,13 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         content = 'file{action create target f1 mode 444}\n'
-        scratch.write_scratchfile(self,'1.man',content)
+        self.write_file('1.man',content)
         man_path = os.path.join(cwd,'1.man')
         run_on_args(['./salve.py','-m',man_path])
         assert os.path.exists(os.path.join(cwd,'f1'))
-        s = scratch.read_scratchfile(self,'f1')
+        s = self.read_file('f1')
         assert s == '', '%s' % s
-        assert scratch.get_scratchfile_mode(self,'f1') == int('444',8)
+        assert self.get_file_mode('f1') == int('444',8)
 
     @istest
     def change_own_permissions(self):
@@ -162,8 +157,42 @@ class TestWithScratchdir():
         """
         cwd = self.scratch_dir
         content = 'file{action create target 1.man mode 066}\n'
-        scratch.write_scratchfile(self,'1.man',content)
+        self.write_file('1.man',content)
         man_path = os.path.join(cwd,'1.man')
         run_on_args(['./salve.py','-m',man_path])
         assert os.path.exists(os.path.join(cwd,'1.man'))
-        assert scratch.get_scratchfile_mode(self,'1.man') == int('066',8)
+        assert self.get_file_mode('1.man') == int('066',8)
+
+    @istest
+    def copy_file_triggers_backup(self):
+        """
+        E2E: Copy a Single File
+
+        Runs a manifest which copies itself and verifies the contents of
+        the destination file.
+        """
+        cwd = self.scratch_dir
+        backup_dir = 'backups'
+        backup_log = 'backup.log'
+        self.make_dir(backup_dir)
+        self.write_file(backup_log,'')
+        self.write_file('f1','')
+
+        content = 'file { action copy source 1.man target f1 '+\
+                  'backup_dir %s ' % backup_dir +\
+                  'backup_log %s ' % backup_log + '}\n'
+
+        self.write_file('1.man',content)
+        man_path = os.path.join(cwd,'1.man')
+        run_on_args(['./salve.py','-m',man_path])
+
+        s = self.read_file('f1')
+        assert s == content, '%s' % s
+        s = self.read_file('backup.log').strip()
+        ss = shlex.split(s)
+        assert len(ss) == 3
+        assert ss[2] == os.path.join(cwd,'f1')
+        backup_path = self.get_backup_path(backup_dir,'f1')
+        backup_path = os.path.join(backup_path,ss[1])
+        s = self.read_file(backup_path)
+        assert s == ''
--- tests/utils/scratch.py
+++ tests/utils/scratch.py
@@ -2,26 +2,29 @@
 
 import os, tempfile, shutil
 
-def make_scratch(obj):
-    obj.scratch_dir = tempfile.mkdtemp()
+class ScratchContainer(object):
+    def setUp(self):
+        self.scratch_dir = tempfile.mkdtemp()
+    def tearDown(self):
+        shutil.rmtree(self.scratch_dir)
 
-def rm_scratch(obj):
-    shutil.rmtree(obj.scratch_dir)
+    def get_backup_path(self,backup_dir,relpath):
+        return os.path.join(self.scratch_dir,backup_dir,
+            os.path.relpath(self.scratch_dir,'/'),relpath)
 
-def make_scratchdir(obj,relpath):
-    full_path = os.path.join(obj.scratch_dir,relpath)
-    if not os.path.exists(full_path):
-        os.makedirs(full_path)
+    def make_dir(self,relpath):
+        full_path = os.path.join(self.scratch_dir,relpath)
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
 
-def write_scratchfile(obj,relpath,content):
-    with open(os.path.join(obj.scratch_dir,relpath),'w') as f:
-        f.write(content)
+    def write_file(self,relpath,content):
+        with open(os.path.join(self.scratch_dir,relpath),'w') as f:
+            f.write(content)
 
-def read_scratchfile(obj,relpath):
-    with open(os.path.join(obj.scratch_dir,relpath),'r') as f:
-        return f.read()
+    def read_file(self,relpath):
+        with open(os.path.join(self.scratch_dir,relpath),'r') as f:
+            return f.read()
 
-def get_scratchfile_mode(obj,relpath):
-    return os.stat(os.path.join(obj.scratch_dir,relpath)).st_mode\
-           & 0777
-
+    def get_file_mode(self,relpath):
+        return os.stat(os.path.join(self.scratch_dir,relpath)).st_mode\
+               & 0777
