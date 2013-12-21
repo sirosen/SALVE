b3a8273f0d4e741621dfd17f2c147bd0859d5319
Stephen Rosen <sirosen@uchicago.edu>
2013-12-21 10:49:21 -0600

    Separated ShellActions from src.execute.action
    Isolates the activities of SAs from base action definition and testing.
    Not strictly necessary, since SAs aren't used at all in SALVE at this
    stage, but good cleanup in preparation for dynamic action def in
    src.execute.action

---
+++ src/execute/shell.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import subprocess
+
+from src.execute.action import Action, ActionException
+
+class ShellAction(Action):
+    """
+    A ShellAction is one of the basic Action types, used to invoke
+    shell subprocesses.
+    """
+    def __init__(self, command, context):
+        """
+        ShellAction constructor.
+
+        Args:
+            @command
+            A string that defines the shell command to execute when the
+            ShellAction is invoked.
+            @context
+            The ShellAction's StreamContext.
+        """
+        Action.__init__(self,context)
+        self.cmd = command
+
+    def __str__(self):
+        return 'ShellAction('+str(self.cmd)+')'
+
+    def execute(self):
+        """
+        ShellAction execution.
+
+        Invokes the ShellAction's command, and fails if it returns a
+        nonzero exit code, and returns its stdout and stderr.
+        """
+        # run the command, passing output to PIPE
+        process = subprocess.Popen(self.cmd,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   shell=True)
+        # Popen is asynchronous without an invocation of wait()
+        process.wait()
+        # check if returncode became nonzero, and fail if it did
+        if process.returncode != 0:
+            raise ActionException(str(self)+\
+                ' failed with exit code '+str(process.returncode),
+                self.context)
+
+        return process.communicate()
--- tests/unit/execute/action_test.py
+++ tests/unit/execute/action_test.py
@@ -8,14 +8,6 @@ from src.util.error import StreamContext
 
 import src.execute.action as action
 
-class MockProcess(object):
-    def __init__(self):
-        self.returncode = 0
-    def wait(self):
-        pass
-    def communicate(self):
-        return None,None
-
 dummy_context = StreamContext('no such file',-1)
 
 @istest
@@ -44,36 +36,6 @@ def empty_action_list():
     assert len(done_actions) == 0
 
 @istest
-def empty_shell_action():
-    """
-    Action Shell Empty Command List Is No-Op
-    Verifies that executing an empty ShellAction does nothing.
-    """
-    # ensures that empty shell actions are valid (but silly)
-    a = action.ShellAction([],dummy_context)
-
-@istest
-def shell_action_basic():
-    """
-    Action Shell Singleton Command List
-    Verifies that executing a ShellAction with one command runs that
-    command.
-    """
-    # we patch Popen to prevent an attempt to actually invoke the
-    # commands passed in
-    # instead, commands are stored in the done_commands list
-    done_commands = []
-    def mock_Popen(commands, stdout=None, stderr=None, shell=None):
-        done_commands.append(commands)
-        return MockProcess()
-
-    with patch('subprocess.Popen',mock_Popen):
-        a = action.ShellAction('mkdir /a/b',dummy_context)
-        a.execute()
-
-    assert done_commands[0] == 'mkdir /a/b'
-
-@istest
 def action_list_inorder():
     """
     Action List Execute In Order
@@ -81,49 +43,17 @@ def action_list_inorder():
     specified order.
     """
     done_actions = []
-    # this ref is used to avoid a recursive loop, since we patch
-    # execute with mock_execute
-    real_execute = action.ShellAction.execute
-    def mock_execute(self):
-        real_execute(self)
-        done_actions.append(self)
 
-    # we patch Popen to prevent an attempt to actually invoke the
-    # commands passed in
-    # instead, commands are stored in the done_commands list
-    done_commands = []
-    def mock_Popen(commands, stdout=None, stderr=None, shell=None):
-        done_commands.append(commands)
-        return MockProcess()
+    class DummyAction(action.Action):
+        def __init__(self,ctx):
+            action.Action.__init__(self,ctx)
+        def execute(self):
+            done_actions.append(self)
 
-    with patch('src.execute.action.ShellAction.execute',mock_execute):
-        with patch('subprocess.Popen',mock_Popen):
-            a = action.ShellAction('a b',dummy_context)
-            b = action.ShellAction('p q r',dummy_context)
-            al = action.ActionList([a,b],dummy_context)
-            al.execute()
+    a = DummyAction(dummy_context)
+    b = DummyAction(dummy_context)
+    al = action.ActionList([a,b],dummy_context)
+    al.execute()
 
     assert done_actions[0] == a
     assert done_actions[1] == b
-    assert done_commands[0] == 'a b'
-    assert done_commands[1] == 'p q r'
-
-@istest
-def failed_shell_action():
-    """
-    Action Shell Failure Raises Exception
-    Verifies that executing a ShellAction raises an exception if one of
-    the commands in it returns a nonzero status.
-    """
-    def mock_Popen(commands, stdout=None, stderr=None, shell=None):
-        """
-        Produces a dummy failure process. We don't bother about the
-        done_commands list here because it is not relevant.
-        """
-        p = MockProcess()
-        p.returncode = 1
-        return p
-
-    with patch('subprocess.Popen',mock_Popen):
-        a = action.ShellAction(['touch /a/b'],dummy_context)
-        ensure_except(action.ActionException,a.execute)
